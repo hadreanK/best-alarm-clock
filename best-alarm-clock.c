7067ecd97decf30d92562673bb32c38968d62e8a
Adrian Kirn <Adrian@adrian-kirns-macbook-2.local>
2016-11-13 14:36:06 -0800

    Holy crap it's becaues I used dashes (-) instead of underscores (_) in the ifndef statement

---
+++ best-alarm-clock.c
@@ -0,0 +1,21 @@
+/*
+This is just a test
+*/
+#ifndef best_alarm_clock_cpp
+#define best_alarm_clock_cpp
+
+// INCLUDES
+	
+	#include "best-alarm-clock.h"
+	//#include "LED_Clock_helper.h"
+	#include "Arduino.h"
+	//#include <LiquidCrystal.h>
+	//#include <OneWire.h>    // required for thermistor
+	//#include <DallasTemperature.h>    // required for thermistor
+	
+void helloWorld(menuInfo *menuP){
+
+	
+}
+
+#endif
--- best-alarm-clock.h
+++ best-alarm-clock.h
@@ -1,15 +1,8 @@
 /*
 This is just a test
 */
-#ifndef best-alarm-clock_h
-#define best-alarm-clock_h
-
-	#include "LED_Clock_helper.h"
-	#include "Arduino.h"
-	#include <LiquidCrystal.h>
-	#include <OneWire.h>    // required for thermistor
-	#include <DallasTemperature.h>    // required for thermistor
-	
+#ifndef best_alarm_clock_h
+#define best_alarm_clock_h	
 
 
 typedef struct{
@@ -19,14 +12,6 @@ typedef struct{
 	char botLine[16]; // Contains the characters to display in the bottom line
 } menuInfo;
 	
-void preSetupEverything (int LCD, int LCDStartPin, int Thermometer, int thermoPin);
-
-void setupEverything (int LCD, int Thermometer, int thermoPin);
-
-void setMenu(void); //menuInfo *mp); //, clockInfo *cp);
-// This function will 
-// Note: x and y must both be positive 
-// Increasing x goes to the right (->) in the menu
-// Increasing y goes down in the menu
+void helloWorld(menuInfo *menuP);
 
 #endif
